6a2da4ee9b994bf55d99823e8fcb32b72f5b47ea
修家絃喜 <genki561907@icloud.com>
2025-06-28 18:33:30 +0900

    apiClientを少し変更

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 //api叩く際に共通となる部分
 const apiClient = axios.create({
     //baseURL: "http://localhost:8000/api",
-    baseURL: process.env.NEXT_PUBLIC_API_URL,
+    baseURL: "process.env.NEXT_PUBLIC_API_URL",
     headers: {
         "Content-Type": "application/json"
     }
