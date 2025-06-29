5ab09522ca9446da3e4d8cde72734095049184cb
修家絃喜 <genki561907@icloud.com>
2025-06-29 10:59:15 +0900

    force-dynamicを追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import PostList from "@/component/Postlist";
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   return (
     <>
--- src/component/Postlist.tsx
+++ src/component/Postlist.tsx
@@ -2,8 +2,6 @@ import { PostContent } from "@/types";
 import Post from "./Post";
 import Sidebar from "./Sidebar";
 
-export const dynamic = "force-dynamic";
-
 //SSRでデータを取得するための関数
 const getPosts = async (): Promise<PostContent[]> => {
   console.log("NEXT_PUBLIC_API_URL:", process.env.NEXT_PUBLIC_API_URL);
