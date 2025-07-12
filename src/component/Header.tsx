06fe0c978a056db1d579c621aecd6260404c6e4a
修家絃喜 <genki561907@icloud.com>
2025-07-12 19:38:21 +0900

    レスポンシブ追加

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -4,7 +4,7 @@ import React, { FC } from "react";
 const Header: FC = () => {
   return (
     <>
-      <header className="bg-[#0c0a28] text-white mb-0 h-24 whitespace-nowrap px-8">
+      <header className="bg-[#0c0a28] text-white mb-0 h-24 px-8">
         <div className="flex justify-between pt-8 items-center">
           <h1 className="text-3xl font-bold">求人検索アプリ</h1>
           <div className="flex gap-10 text-white cursor-pointer">
